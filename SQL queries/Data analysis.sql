87f7594e8b121c1edb285356ac1c24493723417c
Ajaikumar A <ajaikumar2038@gmail.com>
2023-03-15 13:13:03 +0530

    Update Data analysis.sql

--- SQL queries/Data analysis.sql
+++ SQL queries/Data analysis.sql
@@ -98,26 +98,26 @@ GROUP BY
 
 -- Select the year and the average number of orders per customer for each year from the results of the subquery "repeat_orders".
 SELECT
-	year,
-	AVG(no_of_orders) avg_orders_by_customer
+  year,
+  AVG(no_of_orders) avg_orders_by_customer
 FROM
 (
 -- This subquery finds the number of orders for each unique customer and year by joining the customers and orders tables on the customer_id column.
 -- It groups the results by year and customer_unique_id and counts the number of orders for each group.
 	SELECT
-		DATEPART(YEAR, o.order_purchase_timestamp) year,
-		c.customer_unique_id,
-		COUNT(o.order_id) no_of_orders
+	  DATEPART(YEAR, o.order_purchase_timestamp) year,
+	  c.customer_unique_id,
+	  COUNT(o.order_id) no_of_orders
 	FROM
-		customers c
-		JOIN orders o
-			ON c.customer_id = o.customer_id
+	  customers c
+	  JOIN orders o
+	    ON c.customer_id = o.customer_id
 	GROUP BY
-		DATEPART(YEAR, o.order_purchase_timestamp),
-		c.customer_unique_id
+	  DATEPART(YEAR, o.order_purchase_timestamp),
+	  c.customer_unique_id
 ) repeat_orders
 GROUP BY
-	year;
+  year;
 
 
 /* Summary */
@@ -127,108 +127,108 @@ GROUP BY
 WITH active_users AS
   ( 
 	SELECT
-		year,
-		ROUND(AVG(total_customers), 0) avg_active_users
+	  year,
+	  ROUND(AVG(total_customers), 0) avg_active_users
 	FROM
 	  (
 		SELECT
-			DATEPART(YEAR, o.order_purchase_timestamp) year,
-			DATEPART(MONTH, o.order_purchase_timestamp) month,
-			COUNT(DISTINCT(c.customer_unique_id)) total_customers
+  		  DATEPART(YEAR, o.order_purchase_timestamp) year,
+		  DATEPART(MONTH, o.order_purchase_timestamp) month,
+		  COUNT(DISTINCT(c.customer_unique_id)) total_customers
 		FROM
-			orders o 
-			  JOIN customers c 
-				ON o.customer_id = c.customer_id
+		  orders o 
+		  JOIN customers c 
+		  ON o.customer_id = c.customer_id
 		GROUP BY
-			DATEPART(YEAR, o.order_purchase_timestamp),
-			DATEPART(MONTH, o.order_purchase_timestamp)
+		  DATEPART(YEAR, o.order_purchase_timestamp),
+		  DATEPART(MONTH, o.order_purchase_timestamp)
 	  ) total_users
 	GROUP BY
-		year
+	  year
   ),
   new_users AS
   (
 	SELECT
-		DATEPART(YEAR, first_order) year,
-		COUNT(DISTINCT(customer_unique_id)) new_customers
+	  DATEPART(YEAR, first_order) year,
+	  COUNT(DISTINCT(customer_unique_id)) new_customers
 	FROM
 	  (
 		SELECT
-			c.customer_unique_id,
-			MIN(o.order_purchase_timestamp) first_order
+		  c.customer_unique_id,
+		  MIN(o.order_purchase_timestamp) first_order
 		FROM
-			customers c
-			  JOIN orders o
-				ON c.customer_id = o.customer_id
+		  customers c
+		  JOIN orders o
+		  ON c.customer_id = o.customer_id
 		GROUP BY
-			c.customer_unique_id
+		  c.customer_unique_id
 	  ) first_purchase
 	GROUP BY 
-		DATEPART(YEAR, first_order)
+	  DATEPART(YEAR, first_order)
 	),
 	repeat_orders AS
 	(
 		SELECT
-			year,
-			COUNT(customer_unique_id) customers_with_repeat_orders
+		  year,
+		  COUNT(customer_unique_id) customers_with_repeat_orders
 		FROM
 		  (
 			SELECT
-				DATEPART(YEAR, o.order_purchase_timestamp) year,
-				c.customer_unique_id,
-				COUNT(o.order_id) no_of_orders
+			  DATEPART(YEAR, o.order_purchase_timestamp) year,
+			  c.customer_unique_id,
+			  COUNT(o.order_id) no_of_orders
 			FROM
-				customers c
-				JOIN orders o
-				  ON c.customer_id = o.customer_id
+			  customers c
+			  JOIN orders o
+			    ON c.customer_id = o.customer_id
 			GROUP BY 
-				DATEPART(YEAR, o.order_purchase_timestamp),
-				c.customer_unique_id
+			  DATEPART(YEAR, o.order_purchase_timestamp),
+			  c.customer_unique_id
 			HAVING 
-				COUNT(o.order_id) > 1
+			  COUNT(o.order_id) > 1
 		  ) ro
 		GROUP BY
-			year
+		  year
 	),
 	average_orders AS
 	(
 		SELECT
-			year,
-			AVG(no_of_orders) avg_orders_by_customer
+		  year,
+		  AVG(no_of_orders) avg_orders_by_customer
 		FROM
 		  (
 			SELECT
-				DATEPART(YEAR, o.order_purchase_timestamp) year,
-				c.customer_unique_id,
-				COUNT(o.order_id) no_of_orders
+			  DATEPART(YEAR, o.order_purchase_timestamp) year,
+			  c.customer_unique_id,
+			  COUNT(o.order_id) no_of_orders
 			FROM
-				customers c
-				JOIN orders o
-				  ON c.customer_id = o.customer_id
+			  customers c
+			  JOIN orders o
+			    ON c.customer_id = o.customer_id
 			GROUP BY 
-				DATEPART(YEAR, o.order_purchase_timestamp),
-				c.customer_unique_id
+			  DATEPART(YEAR, o.order_purchase_timestamp),
+			  c.customer_unique_id
 		  ) repeat_orders
 		GROUP BY
-			year
+		  year
 	)
 
 SELECT
-	a.year,
-	a.avg_active_users,
-	n.new_customers,
-	r.customers_with_repeat_orders,
-	av.avg_orders_by_customer
+  a.year,
+  a.avg_active_users,
+  n.new_customers,
+  r.customers_with_repeat_orders,
+  av.avg_orders_by_customer
 FROM
-	active_users a 
-	JOIN new_users n 
-		ON a.year = n.year
-	JOIN repeat_orders r 
-		ON a.year = r.year
-	JOIN average_orders av 
-		ON a.year = av.year
+  active_users a 
+  JOIN new_users n 
+    ON a.year = n.year
+  JOIN repeat_orders r 
+    ON a.year = r.year
+  JOIN average_orders av 
+    ON a.year = av.year
 ORDER BY
-	year desc;
+  year desc;
 ---------------------------------------------------------------------------------------------------------------------------------------------------
 
 
@@ -299,11 +299,8 @@ FROM
      DATEPART(YEAR, o.order_purchase_timestamp) year,
      p.product_category_name product_category,
      ROUND(SUM(oi.price + oi.freight_value), 2) revenue,
-     RANK() OVER
-           (
-            PARTITION BY DATEPART(YEAR, o.order_purchase_timestamp)
-            ORDER BY ROUND(SUM(oi.price + oi.freight_value), 2) DESC
-           ) AS ranking
+     RANK() OVER (PARTITION BY DATEPART(YEAR, o.order_purchase_timestamp) 
+		   ORDER BY ROUND(SUM(oi.price + oi.freight_value), 2) DESC) AS ranking
    FROM
      products p
      JOIN order_items oi
@@ -335,11 +332,8 @@ FROM
      DATEPART(YEAR, o.order_purchase_timestamp) year,
      p.product_category_name product_category,
      COUNT(o.order_id) canceled_orders,
-     RANK() OVER
-            (
-             PARTITION BY DATEPART(YEAR, o.order_purchase_timestamp)
-             ORDER BY COUNT(o.order_id) DESC
-            ) ranking
+     RANK() OVER (PARTITION BY DATEPART(YEAR, o.order_purchase_timestamp)
+                   ORDER BY COUNT(o.order_id) DESC) ranking
   FROM
     orders o
      JOIN order_items oi
@@ -364,120 +358,114 @@ ORDER BY
 WITH revenue_per_year AS
  (
 	SELECT
-		DATEPART(YEAR, o.order_purchase_timestamp) year,
-		ROUND(SUM(rev.revenue_per_order), 2) revenue
+	  DATEPART(YEAR, o.order_purchase_timestamp) year,
+	  ROUND(SUM(rev.revenue_per_order), 2) revenue
 	FROM
 	  (
 		SELECT
-			order_id,
-			ROUND(SUM(price + freight_value), 2) revenue_per_order
+		  order_id,
+		  ROUND(SUM(price + freight_value), 2) revenue_per_order
 		FROM 
-			order_items
+		  order_items
 		GROUP BY
-			order_id
+		  order_id
 	  ) rev
-			JOIN orders o
-			  ON rev.order_id = o.order_id
+	  JOIN orders o
+	    ON rev.order_id = o.order_id
 	WHERE
-		o.order_status = 'delivered'
+	  o.order_status = 'delivered'
 	GROUP BY
-		DATEPART(YEAR, o.order_purchase_timestamp)
+	  DATEPART(YEAR, o.order_purchase_timestamp)
 
  ),
  canceled_orders_per_year AS
  (
 	SELECT
-		DATEPART(YEAR, order_purchase_timestamp) year,
-		COUNT(DISTINCT order_id) canceled_orders
+	  DATEPART(YEAR, order_purchase_timestamp) year,
+	  COUNT(DISTINCT order_id) canceled_orders
 	FROM
-		orders
+	  orders
 	WHERE 
-		order_status = 'canceled'
+	  order_status = 'canceled'
 	GROUP BY
-		DATEPART(YEAR, order_purchase_timestamp)
+	  DATEPART(YEAR, order_purchase_timestamp)
 	
   ),
   best_selling_category AS
   (
 	SELECT
-		year,
-		product_category,
-		revenue 
+	  year,
+	  product_category,
+	  revenue 
 	FROM
 	  (
 		SELECT 
-			DATEPART(YEAR, o.order_purchase_timestamp) year,
-			p.product_category_name product_category,
-			ROUND(SUM(oi.price + oi.freight_value), 2) revenue,
-			RANK() OVER
-					(
-					  PARTITION BY DATEPART(YEAR, o.order_purchase_timestamp)
-					  ORDER BY ROUND(SUM(oi.price + oi.freight_value), 2) DESC
-					) AS ranking 
+		  DATEPART(YEAR, o.order_purchase_timestamp) year,
+		  p.product_category_name product_category,
+		  ROUND(SUM(oi.price + oi.freight_value), 2) revenue,
+		  RANK() OVER (PARTITION BY DATEPART(YEAR, o.order_purchase_timestamp)
+				ORDER BY ROUND(SUM(oi.price + oi.freight_value), 2) DESC) AS ranking 
 		FROM
-			products p
-			  JOIN order_items oi
-				ON p.product_id = oi.product_id
-			  JOIN orders o 
-				ON oi.order_id = o.order_id
+		  products p
+		  JOIN order_items oi
+		    ON p.product_id = oi.product_id
+		  JOIN orders o 
+		    ON oi.order_id = o.order_id
 		GROUP BY
-			DATEPART(YEAR, o.order_purchase_timestamp),
-			p.product_category_name
+		  DATEPART(YEAR, o.order_purchase_timestamp),
+		  p.product_category_name
 	  ) revenue_rank
 	WHERE
-		ranking = 1
+	  ranking = 1
 
   ),
   most_canceled_category AS 
   (
 	SELECT
-		year,
-		product_category,
-		canceled_orders
+	  year,
+	  product_category,
+	  canceled_orders
 	FROM 
 	  (
 		SELECT
-			DATEPART(YEAR, o.order_purchase_timestamp) year,
-			p.product_category_name product_category,
-			COUNT(o.order_id) canceled_orders,
-			RANK() OVER
-					(
-					  PARTITION BY DATEPART(YEAR, o.order_purchase_timestamp)
-					  ORDER BY COUNT(o.order_id) DESC
-					) ranking
+		  DATEPART(YEAR, o.order_purchase_timestamp) year,
+		  p.product_category_name product_category,
+		  COUNT(o.order_id) canceled_orders,
+		  RANK() OVER (PARTITION BY DATEPART(YEAR, o.order_purchase_timestamp)
+				ORDER BY COUNT(o.order_id) DESC) ranking
 		FROM 
-			orders o
-			  JOIN order_items oi
-				ON o.order_id = oi.order_id
-			  JOIN products p
-				ON oi.product_id = p.product_id
+		  orders o
+		  JOIN order_items oi
+		    ON o.order_id = oi.order_id
+		  JOIN products p
+	  	    ON oi.product_id = p.product_id
 		WHERE
-			o.order_status = 'canceled'
+		  o.order_status = 'canceled'
 		GROUP BY
-			DATEPART(YEAR, order_purchase_timestamp),
-			p.product_category_name
+		  DATEPART(YEAR, order_purchase_timestamp),
+		  p.product_category_name
 	  ) cancels
 	WHERE
-		ranking = 1
+	  ranking = 1
 	
   )
 
 SELECT
-	r.year,
-	r.revenue,
-	c.canceled_orders,
-	b.product_category best_selling_category,
-	b.revenue best_selling_revenue,
-	m.product_category most_canceled_category,
-	m.canceled_orders most_canceled_orders
+  r.year,
+  r.revenue,
+  c.canceled_orders,
+  b.product_category best_selling_category,
+  b.revenue best_selling_revenue,
+  m.product_category most_canceled_category,
+  m.canceled_orders most_canceled_orders
 FROM
-	revenue_per_year r
-	JOIN canceled_orders_per_year c
-		ON r.year = c.year
-	JOIN best_selling_category b
-		ON r.year = b.year
-	JOIN most_canceled_category m
-		ON r.year = m.year;
+  revenue_per_year r
+  JOIN canceled_orders_per_year c
+    ON r.year = c.year
+  JOIN best_selling_category b
+    ON r.year = b.year
+  JOIN most_canceled_category m
+    ON r.year = m.year;
 
 
 
@@ -521,11 +509,8 @@ FROM
      DATEPART(YEAR, o.order_purchase_timestamp) year,
      op.payment_type favorite_payment_type,
      COUNT(op.payment_type) no_of_usage,
-     RANK() OVER
-            (
-             PARTITION BY DATEPART(YEAR, o.order_purchase_timestamp)
-             ORDER BY COUNT(op.payment_type) DESC
-            ) payment_ranking
+     RANK() OVER (PARTITION BY DATEPART(YEAR, o.order_purchase_timestamp)
+                   ORDER BY COUNT(op.payment_type) DESC) payment_ranking
    FROM
      order_payments op
       JOIN orders o
